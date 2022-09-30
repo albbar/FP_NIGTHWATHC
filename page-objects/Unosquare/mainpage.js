9534d8c9c7774545f1d8b7c8e4c8ff897c4e1e43
albbar <alcabarme@gmail.com>
2022-09-30 06:27:49 -0500

    type comments over mainpage.js

--- page-objects/Unosquare/mainpage.js
+++ page-objects/Unosquare/mainpage.js
@@ -30,12 +30,13 @@ module.exports = {
       validateName(value){
         let unos = this
         return unos
-        .api.findElements('div>.name', function(elements){
+        .api.findElements('div>.name', function(elements){ //search for multiples elements on the page and returns them as web element JSON objects
           let flag= false
-          elements.value.forEach(function (elementObj, i) {
-            let elementID = elementObj[Object.keys(elementObj)[0]]
-            unos.api.elementIdText(elementID, function (result){      
-              if(value==result.value){
+          elements.value.forEach(function (elementObj, i) { // iterate over the different elements
+            let elementID = elementObj[Object.keys(elementObj)[0]] // get the KEY (ELEMENT) for the JSON objects to get the elementObjt ID
+            unos.api.elementIdText(elementID, function (result){     //get the visible text or innertext of the elements in the call back function using
+              /**For the test we need to know if the "value" is contain in "result.value" */
+              if(value==result.value){ 
                 unos.assert.equal(result.value, value, `${value} exist in the list`)
                 flag = true
               }
